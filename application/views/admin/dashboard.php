8cd13b9ea7d18e1c3c0519a1a85799ba3cf68bf4
KinderJoyEnak-RY <randisyustico25@gmail.com>
2023-10-22 23:35:09 +0700

    testing ke 6

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboardd</title>
+    <title>Admin Dashboard</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <!-- AdminLTE CSS -->
